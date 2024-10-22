4c1db7a3742152a7d316390730fec4864a76e922
Jandalslap <128538609+Jandalslap@users.noreply.github.com>
2024-10-22 14:17:43 +1300

    Initial setup

--- app/src/main/java/com/example/miok_info_app/MainActivity.kt
+++ app/src/main/java/com/example/miok_info_app/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.miok_info_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.recyclerapp1.R
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
