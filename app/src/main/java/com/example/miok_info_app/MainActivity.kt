53d77ceca7a758127701529e137bd00007e67289
Jandalslap <128538609+Jandalslap@users.noreply.github.com>
2024-10-22 22:23:20 +1300

    Added splash screen and disclaimer

--- app/src/main/java/com/example/miok_info_app/MainActivity.kt
+++ app/src/main/java/com/example/miok_info_app/MainActivity.kt
@@ -1,13 +1,26 @@
 package com.example.miok_info_app
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.miok_info_app.R
-
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Set up the NavHostFragment
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        return navController.navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
